69b795d89179ac6fe77d068d0b7fe95242d58130
George Plukov <george.plukov@d2l.com>
2021-03-25 09:45:08 -0400

    add overflow group to rollup

--- rollup/rollup-wc.config.js
+++ rollup/rollup-wc.config.js
@@ -50,6 +50,7 @@ const componentFiles = [
 	'./node_modules/@brightspace-ui/core/components/menu/menu-item-radio.js',
 	'./node_modules/@brightspace-ui/core/components/menu/menu-item-separator.js',
 	'./node_modules/@brightspace-ui/core/components/more-less/more-less.js',
+	'./node_modules/@brightspace-ui/core/components/overflow-group/overflow-group.js',
 	'./node_modules/@brightspace-ui/core/components/status-indicator/status-indicator.js',
 	'./node_modules/@brightspace-ui/core/components/switch/switch.js',
 	'./node_modules/@brightspace-ui/core/components/switch/switch-visibility.js',
