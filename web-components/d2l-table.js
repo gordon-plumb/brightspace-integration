cc05e1230ecaf0343bb789ae12a1f552a3e7d289
Dave Lockhart <dlockhart@users.noreply.github.com>
2021-05-26 20:09:19 -0400

    Using Lit table (#3953)

--- web-components/d2l-table.js
+++ web-components/d2l-table.js
@@ -1,15 +1,8 @@
-import 'd2l-table/d2l-table.js';
-const $_documentContainer = document.createElement('template');
+import { tableStyles } from '@brightspace-ui/core/components/table/table-wrapper.js';
 
-$_documentContainer.innerHTML = `<custom-style>
-	<style is="custom-style" include="d2l-table-style">
-		.d2l-grid-mvc > * > tr > td {
-			vertical-align: top;
-		}
-		.d2l-grid-mvc > * > tr > th {
-			vertical-align: middle;
-		}
-	</style>
-</custom-style>`;
-
-document.head.appendChild($_documentContainer.content);
+if (document.getElementById('d2l-table-style-shared') === null) {
+	const style = document.createElement('style');
+	style.id = 'd2l-table-style-shared';
+	style.appendChild(document.createTextNode(tableStyles.cssText));
+	document.head.appendChild(style);
+}
