1f89446fb8e9a2b0459ae173c05f655f4e94cd49
Devon Powell <Devon.Powell@D2L.com>
2021-08-11 12:54:56 -0400

    Add back deduping for local dev BSI runs (#4489)
    * Add back deduping for local BSI runs
    
    This re-adds deduping but getting a list of all package names and adding them to
    the list of dedupable packages. Not having this causes some complications with,
    at least my teams, local workflows.
    
    * Adjust file type check
    
    * Lint fixes
    
    * Switch to file path instead of process.cwd()
    
    * Switch to simpler method that es-dev-server used

--- web-dev-server-dev.config.mjs
+++ web-dev-server-dev.config.mjs
@@ -25,5 +25,15 @@ export default {
 		}
 	],
 	port: parseInt(process.env.npm_config_port) || 8080,
-	preserveSymlinks: true
+	preserveSymlinks: true,
+	nodeResolve: {
+		/*
+		 * this mimics what es-dev-server did for deduping. This works because currently inside node-resolve they take the dedupe
+		 * option and make it a function to run internally. If it's already a function they do nothing and just execute it later.
+		 * https://github.com/open-wc/es-dev-server/blob/98dd47464c97c5ab5fc6ef86723b3f529bc8265c/src/config.ts#L300
+		 * https://github.com/rollup/plugins/blob/b393bcb6fe600e27a33b643a20340dc5669d1f85/packages/node-resolve/src/index.js#L59
+		 * https://github.com/rollup/plugins/blob/b393bcb6fe600e27a33b643a20340dc5669d1f85/packages/node-resolve/src/index.js#L81
+		 */
+		dedupe: importee => !['.', '/'].includes(importee[0])
+	}
 };
