cf4ece6bdcd9222f5b835696b8cf8824d2b980c1
Grant Cleary <Grant.Cleary@D2L.com>
2021-11-17 11:03:27 -0500

    Align html-block renderers with new format

--- web-components/d2l-html-block.js
+++ web-components/d2l-html-block.js
@@ -1,7 +1,7 @@
 import '@brightspace-ui/core/components/html-block/html-block.js';
-import { htmlBlockUserProfileRenderer } from '@brightspace-ui-labs/user-profile-card/user-profile-html-block.js';
+import { HtmlBlockUserProfileRenderer } from '@brightspace-ui-labs/user-profile-card/user-profile-html-block.js';
 import { provideInstance } from '@brightspace-ui/core/mixins/provider-mixin.js';
 
 provideInstance(document, 'html-block-renderers', [
-	htmlBlockUserProfileRenderer
+	new HtmlBlockUserProfileRenderer()
 ]);
